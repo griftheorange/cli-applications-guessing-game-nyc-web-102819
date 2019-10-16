765fa7b60c85fa1a5994794324f30829bb3803b6
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 04:30:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,20 +1,15 @@
 def run_guessing_game
-  numb = rand(5)+1
-  prompt_user
-  if compare_user_to_comp(numb)
-    puts "You guessed the correct number!"
+  comp_guess = rand(5)+1
+  user_guess = prompt_user
+  if user_guess == comp_guess
+    
   else
-    "Sorry! The computer guessed"
+    
   end
+  
 end
 
 def prompt_user
   puts "Please guess a whole number between 1 and 6 inclusive."
   gets.chomp
-end
-
-def compare_user_to_comp(user_guess)
-  comp_guess = rand(5)+1 
-  user_guess == comp_guess
-  
 end
