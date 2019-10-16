ef26b45828d0f03fe56db26947e943c740276fc5
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 04:41:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,6 @@
 def run_guessing_game
   comp_guess = rand(5)+1
-  user_guess = prompt_user.to_i
-  puts comp_guess
-  puts user_guess
+  user_guess = prompt_user
   if user_guess == 'exit'
     puts "Goodbye!"
   elsif user_guess == comp_guess
