eb247f7caab4e3c7f0c73223efd0b6cf2ef46a80
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 04:38:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,7 @@
 def run_guessing_game
-  comp_guess = rand(5)+1
-  user_guess = prompt_user
-  puts user_guess == comp_guess
   if user_guess == 'exit'
     puts "Goodbye!"
-  elsif user_guess == comp_guess
+  elsif user_guess == rand(5)+2
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{comp_guess}."
