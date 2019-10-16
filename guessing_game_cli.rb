81c682ca11fe0a49e4a038e4a11e2dcf4c6968a0
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 04:27:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,8 +1,14 @@
 def run_guessing_game
   numb = rand(5)+1
+  prompt_user
+  
 end
 
 def prompt_user
-  puts "Please guess a number between 1 and 6 inclusive."
+  puts "Please guess a whole number between 1 and 6 inclusive."
   gets.chomp
+end
+
+def compare_user_to_comp(user_guess)
+  user_guess == rand(5)+1
 end
